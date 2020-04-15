badbc8d4983138426e18a67d2a3d046c05c3abbf
c2chack20 <c2chack20@gmail.com>
2020-04-15 20:18:15 +0530

    Added repository pattern

--- NeedyBuddy/Data/ApplicationDbContext.cs
+++ NeedyBuddy/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NeedyBuddy.Data.Model;
 
 namespace NeedyBuddy.Data
 {
@@ -12,5 +13,6 @@ namespace NeedyBuddy.Data
             : base(options)
         {
         }
+        public DbSet<Post> Post { get; set; }
     }
 }
--- NeedyBuddy/Startup.cs
+++ NeedyBuddy/Startup.cs
@@ -30,6 +30,7 @@ namespace NeedyBuddy
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IRepository, Repository<ApplicationDbContext>>();
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
