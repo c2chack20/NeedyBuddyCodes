10a8064affbcb7c7aba1ecbcfcd2903256743637
c2chack20 <c2chack20@gmail.com>
2020-04-16 12:16:57 +0530

    new comment

--- NeedyBuddy/Controllers/PostsController.cs
+++ NeedyBuddy/Controllers/PostsController.cs
@@ -12,6 +12,7 @@ namespace NeedyBuddy.Controllers
 {
     public class PostsController : Controller
     {
+        //dependency injection
         private readonly ApplicationDbContext _context;
         private readonly IRepository _repository;
         public PostsController(ApplicationDbContext context, IRepository repository)
