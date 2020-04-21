a204694b9c045e61718c7a4fcfa449a2cfe1b753
c2chack20 <c2chack20@gmail.com>
2020-04-21 12:49:20 +0530

    My local

---
+++ NeedyBuddy/Data/Migrations/20200421064943_firstmigration.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NeedyBuddy.Data.Migrations
+{
+    public partial class firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Area",
+                table: "RegisterModel");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "RegisterModel");
+
+            migrationBuilder.DropColumn(
+                name: "Contact",
+                table: "RegisterModel");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "RegisterModel");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "RegisterModel");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "RegisterModel");
+
+            migrationBuilder.DropColumn(
+                name: "ModfiedOn",
+                table: "RegisterModel");
+
+            migrationBuilder.DropColumn(
+                name: "Pincode",
+                table: "RegisterModel");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "RegisterModel",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "RegisterModel",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pincode",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserType",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "ServiceCategory",
+                columns: table => new
+                {
+                    ServiceCategoryId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServiceName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceCategory", x => x.ServiceCategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Service",
+                columns: table => new
+                {
+                    ServiceId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServiceCategoryId = table.Column<long>(nullable: true),
+                    Descriptions = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Service", x => x.ServiceId);
+                    table.ForeignKey(
+                        name: "FK_Service_ServiceCategory_ServiceCategoryId",
+                        column: x => x.ServiceCategoryId,
+                        principalTable: "ServiceCategory",
+                        principalColumn: "ServiceCategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Service_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Service_ServiceCategoryId",
+                table: "Service",
+                column: "ServiceCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Service_UserId",
+                table: "Service",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Service");
+
+            migrationBuilder.DropTable(
+                name: "ServiceCategory");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "RegisterModel");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "RegisterModel");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Pincode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Area",
+                table: "RegisterModel",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "RegisterModel",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Contact",
+                table: "RegisterModel",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedOn",
+                table: "RegisterModel",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "RegisterModel",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "RegisterModel",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ModfiedOn",
+                table: "RegisterModel",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Pincode",
+                table: "RegisterModel",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
