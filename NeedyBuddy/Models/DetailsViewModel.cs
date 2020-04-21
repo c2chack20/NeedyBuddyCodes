906911d5ab41e0f42a039c0983fc9274495b2e8c
AnilkumarMaradana <anilkumarmaradana@live.com>
2020-04-21 21:29:03 +0530

    Added user details

--- NeedyBuddy/Models/DetailsViewModel.cs
+++ NeedyBuddy/Models/DetailsViewModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NeedyBuddy.Data.Model
+namespace NeedyBuddy.Models
 {
-    public class UserServicesViewModel
+    public class DetailsViewModel
     {
+
+        public DetailsViewModel()
+        {
+            this.serviceDetailsViewModel = new List<ServiceDetailsViewModel>();
+        }
         public string Id { get; set; }
 
         public string UserName { get; set; }
@@ -20,10 +25,9 @@ namespace NeedyBuddy.Data.Model
 
         public string Pincode { get; set; }
 
-        public string Descriptions { get; set; }
-
         public string ServiceName { get; set; }
 
         public string Address { get; set; }
+        public List<ServiceDetailsViewModel> serviceDetailsViewModel { get; set; }
     }
 }
