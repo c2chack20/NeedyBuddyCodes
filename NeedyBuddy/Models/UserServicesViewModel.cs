b9f36b1a43c84916d41ea5cac30579f49d4c92ba
AnilkumarMaradana <anilkumarmaradana@live.com>
2020-04-21 16:06:19 +0530

    UserService View Model Updated

--- NeedyBuddy/Models/UserServicesViewModel.cs
+++ NeedyBuddy/Models/UserServicesViewModel.cs
@@ -10,6 +10,7 @@ namespace NeedyBuddy.Data.Model
         public string Id { get; set; }
 
         public string UserName { get; set; }
+        public string FirstName { get; set; }
 
         public string ContactNumber { get; set; }
 
